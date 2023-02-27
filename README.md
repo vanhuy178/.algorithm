d0ef19bf23ecb5b39013c1392b1e465eeb7c228f
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-27 14:47:55 +0700

    callback

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# .algorithm
-- Ôn lại thuật toán cho phỏng vấn cũng như cũng cố tư duy trong lập trình.
+# Ôn lại thuật toán cho phỏng vấn cũng như cũng cố tư duy trong lập trình.
+
+## JavaScript
+
+1. array
+2. callback function, hof 
+
--- hof.js
+++ hof.js
@@ -13,12 +13,53 @@ function checkName() {
 // checkName();
 
 // FUNCTION SEQUENCE
+// function myDisplayer(value) {
+//     console.log(value);
+// }
+
+// function myCalculator(num1, num2) {
+//     let sum = num1 + num2;
+//     myDisplayer(sum); // callback
+// }
+// myCalculator(10, 10)
+
+
+
+// USING CALLBACK
+
 function myDisplayer(value) {
     console.log(value);
 }
 
-function myCalculator(num1, num2) {
-    let sum = num1 + num2;
-    myDisplayer(sum); // callback
+function Total(sum1, sum2, callback) {
+    let sum = sum1 + sum2;
+    callback(sum)
 }
-myCalculator(10, 10)
+
+// CALLBACK TO CALL MY CRUSH
+function call(value = '...') {
+    console.log('call to ' + value)
+}
+
+function findNumberToCall(listNumber, callback) {
+    let crush = listNumber.filter(item => item === 18);
+    callback(crush);
+}
+
+findNumberToCall([19, 18, 20], call);
+
+
+// CALLBACK TO REMOVE NEGATIVE NUMBER
+// Create an Array
+const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+function removeNegativeNumber(listNumber, callback) {
+    let arrayPositiveNumber = [];
+    for (let x of myNumbers) {
+        if (callback(x)) {
+            arrayPositiveNumber.push(x)
+        }
+    }
+    return arrayPositiveNumber;
+}
+let positiveNumber = removeNegativeNumber(myNumbers, x => x >= 0);
+console.log(positiveNumber.sort((a, b) => a - b));
