8284f2770a8fb5bc2c81f7ce0ee5f22577a4e4da
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-02-28 19:12:32 +0700

    modifer access C#

--- Person.cs
+++ Person.cs
@@ -1,15 +1,29 @@
 class Person
 {
-    public string firstName;
-    public string lastName;
-    public int age;
+    private string firstName;
+    private string lastName;
+    private byte age;
 
+    public Person()
+    {
+        Console.WriteLine("Initialized the object");
+    }
+
+    public Person(string firstName, string lastName) : this()
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Console.WriteLine("Initialized the first name & last name properties");
+    }
+    public Person(string firstName, string lastName, byte age) : this(firstName, lastName)
+    {
+        this.age = age;
+        Console.WriteLine("Initialized the age property");
+    }
     public string getFullName()
     {
-        return this.firstName + this.lastName;
+        return $"{this.firstName} {this.lastName}";
     }
-    public string sayHi()
-    {
-        return $"Hi, I'm {this.getFullName()}"; // this keyword reference to current object
-    }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -3,15 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Person ps2 = new();
-        Person ps1 = new Person(); // initialize an object
-
-        ps1.firstName = "Huy";
-        ps1.lastName = "Nguyen";
-        ps1.age = 21;
-
-        Console.WriteLine(ps1.getFullName());
-        Console.WriteLine(ps1.sayHi());
+        var p1 = new Person();
+        Console.WriteLine(p1);
 
     }
 }
