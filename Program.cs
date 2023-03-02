e753e2db2b0777e8bcb0efe65922d6b39403b456
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-03-03 01:20:34 +0700

    hashset, dictionary, linked list, queue, stack

--- Program.cs
+++ Program.cs
@@ -206,18 +206,119 @@ class Program
             Age = _age;
         }
     }
+
+
+
     static void Main(string[] args)
     {
+
+        LinkedList<string> listLesson = new LinkedList<string>();
+        var lesson1 = listLesson.AddFirst("Lesson1");
+        var lesson2 = listLesson.AddAfter(lesson1, "Lessin2");
+        var lesson3 = listLesson.AddAfter(lesson2, "Lesson3");
+        var lesson4 = listLesson.AddAfter(lesson3, "Lessin4");
+        var lesson5 = listLesson.AddLast("Lessin5");
+
+        // var node = lesson5;
+        // node = node.Previous;
+        // WriteLine("Element prev is: " + node.Value);
+
+        // foreach (string data in listLesson)
+        // {
+        //     WriteLine("List lesson: " + data);
+        // }
+
+        var node = lesson5;
+        while (node != null)
+        {
+            WriteLine(node.Value);
+            node = node.Previous;
+        }
+
+        Dictionary<string, int> CounterSystem = new Dictionary<string, int>()
+        {
+            ["One"] = 1,
+            ["Two"] = 2,
+            ["Three"] = 3
+
+        };
+
+        CounterSystem.Add("Four", 4);
+        CounterSystem["Five"] = 5;
+
+        foreach (var data in CounterSystem)
+        {
+            WriteLine($"The key: {data.Key},  the value: {data.Value}");
+        }
+
+        // Hashset
+        HashSet<int> evenNumbers = new HashSet<int>();
+        HashSet<int> oddNumbers = new HashSet<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            // Populate numbers with just even numbers.
+            evenNumbers.Add(i * 2);
+
+            // Populate oddNumbers with just odd numbers.
+            oddNumbers.Add((i * 2) + 1);
+        }
+
+        Console.Write("evenNumbers contains {0} elements: ", evenNumbers.Count);
+        DisplaySet(evenNumbers);
+
+        Console.Write("oddNumbers contains {0} elements: ", oddNumbers.Count);
+        DisplaySet(oddNumbers);
+
+        // Create a new HashSet populated with even numbers.
+        HashSet<int> numbers = new HashSet<int>(evenNumbers);
+        Console.WriteLine("numbers UnionWith oddNumbers...");
+        numbers.UnionWith(oddNumbers);
+
+        Console.Write("numbers contains {0} elements: ", numbers.Count);
+        DisplaySet(numbers);
+
+        void DisplaySet(HashSet<int> collection)
+        {
+            Console.Write("{");
+            foreach (int i in collection)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.WriteLine(" }");
+        }
+
+
+
+        Stack<string> inventory = new Stack<string>();
+        // Push into stack
+        inventory.Push("product 1");
+        inventory.Push("product 2");
+        inventory.Push("product 3");
+
+        WriteLine("Count inentory: " + inventory.Count());
+
+        Queue<string> handleProfile = new Queue<string>();
+        //ADD ELEMENT TO ENQUEUE
+        handleProfile.Enqueue("Profile - 1");
+        handleProfile.Enqueue("Profile - 2");
+        handleProfile.Enqueue("Profile - 3");
+        handleProfile.Enqueue("Profile - 4");
+        handleProfile.Enqueue("Profile - 5");
+
+        //REMOVE ELEMENT OUT OF QUEUE BY DEQUE
+        var dequeueFirstElement = handleProfile.Dequeue();// REMOVE THE FIRST ITEM OF QUEUE AND RETURN VALUE OF THE ITEM
+        Console.WriteLine("Browser Profile: " + dequeueFirstElement + " records " + handleProfile.Count());
         User user1 = new User("Nguyen Van Huy", 11);
-        Register(user1.Name, user1.Age);
+        // Register(user1.Name, user1.Age);
 
         // HANDLE EVENT
         UserInput userInput = new UserInput(); // userinput play event --> publisher
         SquareRoot squareRoot = new SquareRoot(); // subcriber
         Squaree squaree = new Squaree();    // subcriber
-        // squareRoot.Sub(userInput);
-        // squaree.Sub(userInput);
-        // userInput.Input();
+                                            // squareRoot.Sub(userInput);
+                                            // squaree.Sub(userInput);
+                                            // userInput.Input();
 
 
         // EXTENSION METHOD
--- Stack.cs
+++ Stack.cs
@@ -1,54 +1,54 @@
-namespace _algorithm;
-class Stack<T>
-{
-    int current = -1; // index at last array
-    T[] items; // craete an array
+// namespace _algorithm;
+// class Stack<T>
+// {
+//     int current = -1; // index at last array
+//     T[] items; // craete an array
 
-    public Stack(int size) // create constructor
-    {
-        items = new T[size]; // create the size of array
-    }
+//     public Stack(int size) // create constructor
+//     {
+//         items = new T[size]; // create the size of array
+//     }
 
-    public bool Empty => current == -1; // check empty
-    public bool Full => current == items.Length - 1; // the current
+//     public bool Empty => current == -1; // check empty
+//     public bool Full => current == items.Length - 1; // the current
 
-    public bool Push(T item)
-    {
-        if (!Full)
-        {
-            items[++current] = item; // ++items the value of x after the operator
-            return true;
-        }
-        return false;
-    }
+//     public bool Push(T item)
+//     {
+//         if (!Full)
+//         {
+//             items[++current] = item; // ++items the value of x after the operator
+//             return true;
+//         }
+//         return false;
+//     }
 
-    public T? Pop() => !Empty ? items[current--] : default(T); // --items the value of x before the operator
-}
-
-
-class NewStack<T>
-{
-    public int current = -1;
-    T[] items;
-
-    public NewStack(int size)
-    {
-        items = new T[size];
-    }
-
-    public bool Empty => current == -1;
-    public bool Full => current == items.Length - 1;
+//     public T? Pop() => !Empty ? items[current--] : default(T); // --items the value of x before the operator
+// }
 
 
-    public bool Push(T item)
-    {
-        if (!Full)
-        {
-            items[++current] = item;
-            return true;
-        }
-        return false;
-    }
+// class NewStack<T>
+// {
+//     public int current = -1;
+//     T[] items;
 
-    public T? Pop() => !Empty ? items[current--] : default(T);
-}
+//     public NewStack(int size)
+//     {
+//         items = new T[size];
+//     }
+
+//     public bool Empty => current == -1;
+//     public bool Full => current == items.Length - 1;
+
+
+//     public bool Push(T item)
+//     {
+//         if (!Full)
+//         {
+//             items[++current] = item;
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     public T? Pop() => !Empty ? items[current--] : default(T);
+// }
